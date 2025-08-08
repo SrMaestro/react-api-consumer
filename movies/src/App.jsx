b21a7be449206eaeb39305dbc93feb2ced260910
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-08-08 19:57:57 -0300

    Criando os componentes pricipais

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   return(
-    <div>
+    <div className='App'>
       <h2>Ola Mundo</h2>
     </div>
   )
--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import './index.css'
+
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Movie from './pages/Movie.jsx'
+import Search from './pages/Search.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
