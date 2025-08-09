81aa603cf811454d7b64e26700fa0569cd15f360
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-08-09 14:37:19 -0300

    before styling

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,10 +1,16 @@
 
+import {Outlet } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+
 import './App.css'
 
 function App() {
   return(
     <div className='App'>
+      <Navbar/>
       <h2>Ola Mundo</h2>
+      <Outlet/>
     </div>
   )
 }
