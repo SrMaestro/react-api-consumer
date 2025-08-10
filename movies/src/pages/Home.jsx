57671e29b79a17dd2112cedaaeb281d3f6289953
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-08-10 15:03:05 -0300

    Estilos de botao de detalhes

--- movies/src/pages/Home.jsx
+++ movies/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"; /* importing a hook from the React library */
 import MovieCard from "../components/MovieCard";
+import "./MoviesGrid.css"
 
 
 const moviesURL = import.meta.env.VITE_API;
