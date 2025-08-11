6886be11db6ff6f4e1f5c26798442001705027e6
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-08-11 15:12:06 -0300

    Filtro de pesquisa

--- movies/src/components/Navbar.jsx
+++ movies/src/components/Navbar.jsx
@@ -1,15 +1,36 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 import "./NavBar.css";
 
 const Navbar = () => {
+
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(search);
+    
+    if(!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("")
+
+    
+  };
+
+
   return (
     <nav id="navbar">
       <h2>
         <Link to="/"><BiCameraMovie/>MoviesLib</Link>
       </h2>
-        <form action="">
-            <input type="text" placeholder="Busque um filme" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)}  
+            value={search}
+            />
             <button type="submit">
                 <BiSearchAlt2/>
             </button>
