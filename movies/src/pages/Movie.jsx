61d84052300f9bb5abab571d84788a57e14d6911
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-08-12 13:50:16 -0300

    Acessando pagina de detalhes

--- movies/src/pages/Movie.jsx
+++ movies/src/pages/Movie.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import "./Movie.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
-  return <div>Home</div>;
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setMovie(data);
+  };
+
+  // const formatCurrency = (number) => {
+  //   return number.toLocaleString("en-US", {
+  //     style: "currency",
+  //     currency: "USD",
+  //   });
+  // };
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    getMovie(movieUrl);
+  }, []);
+
+  return (
+  <div>Detalhes do filme</div>
+  );
 };
 
 export default Movie;
